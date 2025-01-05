7086813e16dc361680ce636687eaae36490353b6
Markus Baier <git@baier-network.de>
2025-01-05 02:07:46 +0100

    Added ebuild for Lyrion Music Server 9.0.0

---
+++ media-sound/lyrionmusicserver/files/lyrionmusicserver.conf.d
@@ -0,0 +1,11 @@
+# /etc/conf.d/lyrionmediaserver
+
+# The following contains any other options you want to specify, such as default
+# logging options.  The example below will prevent the discovery and display of
+# UPnP devices within your players.
+#
+# See "/opt/lyrionmusicserver/slimserver.pl --help" for a full list of possible
+# options, but note that many of them are supplied by
+# /etc/init.d/lyrionmusicserver and so don't need to be present here.
+#LMS_OPTS="--d_startup"
+LMS_OPTS=""
